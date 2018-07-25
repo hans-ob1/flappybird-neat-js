26bccc4d349b9d301993b8f41cb0954d579bf823
khaixcore <haixun00@gmail.com>
2018-07-26 00:31:44 +0800

    added prototype neurons

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flappy bird ai
+# using NEAT
--- testinput.js
+++ testinput.js
@@ -1,42 +1,108 @@
 //reference: https://threads-iiith.quora.com/Neuro-Evolution-with-Flappy-Bird-Genetic-Evolution-on-Neural-Networks
 
+/*
+ input:  height_bird = height of the bird
+         dist_to_pipe = distance to nearest pipe
+         height_pipe = Height of the nearest pipe
 
-var totalModels = 5;
-var LEARNING_RATE = 0.01;
-var model_pool = [];
+ hidden_weights: arr[7][3]
+ output_weights: arr[1][7]
+ activation_function: f(x) = (1/(1-e^-x))
 
-//create a model
-var i;
-for (i = 0; i < 5; i++){
-    const model = tf.sequential();
-    model.add(tf.layers.dense({units: 7,
-                            activation: 'sigmoid',
-                            inputDim: 3
-                            }));
-    model.add(tf.layers.dense({units: 1,
-                            activation: 'sigmoid',
-                            }));
-    const optimizer = tf.train.sgd(LEARNING_RATE);
-    model.compile({optimizer, loss: 'meanSquaredError', metrics: ["accuracy"]});
-    model_pool.push(model);
+ initalize weights: 1/sqrt(n), where n is the number of hidden neurons
+*/
+
+
+var numModels = 50;
+var numInputs = 3;
+var numHiddenNeurons = 7;
+var numOutputs = 1;
+
+// random number generator based on number of hidden neuron
+function randGen(n){
+    var upperRange = 1/Math.sqrt(n);
+    var randomNum = Math.random()*upperRange;
+    if (Math.random() > 0.5){
+        randomNum = -randomNum;
+    }
+
+    return randomNum;
 }
 
-function crossover(modexA, modexB){
-    var weightsA = model_pool[modexA].layers[0].getWeights();
-    var weightsB = model_pool[modexB].layers[0].getWeights();
 
-    console.log(weightsA);
+// construct hidden layer template
+function construct_hidden(numIn, numHidden){
+    var i;
+    var j;
 
-    var newWeightsA = weightsA;
-    var newWeightsB = weightsB;
+    var hidden_layer = [];
+    for (i = 0; i < numHidden; i++){
+        var slot = []
+        for (j = 0; j < numIn; j++){
+            slot.push(randGen(numHidden));
+        }
+        hidden_layer.push(slot);
+    }
 
-    newWeightsA[0] = weightsB[0];
-    newWeightsB[0] = weightsA[0];
+    return hidden_layer;
+}
+
+// construct output layer template
+function construct_output(numOut,numHidden){
+    var i;
+    var j;
+
+    var output_layer = [];
+    // more than one output
+    for (i = 0; i < numOut; i++){
+        var slot = [];
+        for (j = 0; j < numHidden; j++){
+            slot.push(randGen(numHidden));
+        }
+        output_layer.push(slot);
+    }
+    return output_layer;
+}
+
+
+function Model(hidden_layer,output_layer){
+
+    this.hiddenLayer = hidden_layer;
+    this.outputLayer = output_layer;
     
-    model_pool[modexA].layers[0].setWeights(newWeightsA);
-    model_pool[modexB].layers[0].setWeights(newWeightsB);
 }
 
-function mutate(weights){
-    crossover(0,1);
+Model.prototype.feedforward = function(inputArray){
+
+    var i;
+    var j;
+
+    var hidden_neuron_output = [];
+    var final_node_output = [];
+
+    for (i = 0; i < this.hiddenLayer.length; i++){
+
+        var sum_of_weights = 0;
+        for (j = 0; j < inputArray.length; j++){
+            sum_of_weights += this.hiddenLayer[i][j]*inputArray[j];
+        }
+
+        //sigmoid
+        hidden_neuron_output.push(1/(1+Math.exp(-sum_of_weights)));
+    }
+
+    console.log(hidden_neuron_output);
+}
+
+
+
+function testFunc(){
+
+    var hiddenlayer = construct_hidden(numInputs, numHiddenNeurons);
+    var outputlayer = construct_output(numOutputs, numHiddenNeurons);
+
+    test = new Model(hiddenlayer,outputlayer);
+
+    test.feedforward([3,3,3]);
+
 }
