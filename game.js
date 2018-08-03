285271a5065259c106b40cb7a25ee01b27e7b652
khaixcore <haixun00@gmail.com>
2018-08-04 01:56:24 +0800

    switched to p5.js

--- game.js
+++ game.js
@@ -1,63 +1,104 @@
-// game engine
-// https://phaser.io/tutorials/making-your-first-phaser-3-game/part7
+/* main game engine */
 
-var config = {
-    width: Params.frame.FRAME_WIDTH,
-    height: Params.frame.FRAME_HEIGHT,
-    renderer: Phaser.AUTO,
-    physics: {
-        default: 'arcade',
-    },
-    parent: 'game-container',
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+// Initialize params
+var params = {
+  FRAME_WIDTH: 336,
+  FRAME_HEIGHT: 512,
+  FRAME_RATE: 30,
+
+  GROUND_X: 0,
+  GROUND_Y: 490,
+
+  SCORE_Y: 20,
+  SCORE_WIDTH: 24,
+  SCORE_SPACE: 2,
+
+  PIPES_NUM: 10,
+  PIPE_WIDTH: 52,
+  PIPE_HEIGHT: 500,
+  PIPE_MIN_Y: 100,
+  PIPE_MAX_Y: 300,
+  PIPE_SPEED: -3,
+  PIPE_GAP: 100,
+
+  BIRD_NUM: 1,
+  BIRD_X: 100,
+  BIRD_Y: 200,
+  BIRD_RADIUS: 48,
+
+  FLAP_GAIN: -10,
+  MAX_FALL_SPEED: 10,
+  ACCEL: 1
 };
 
-const game = new Phaser.Game(config);
+var game_state = 'prestart';
+//var flap_gain = createVector(0, 5); // amount of height gain with a single flap
+var birds = [];
+var pipes = [];
 
-function preload() {
-    // game environment
-    this.load.image('background','assets/background.png');
-    this.load.image('platform','assets/platform.png');
+function setup() {
+  // load background
+  bg = loadImage("assets/background.png");
+  grd = loadImage("assets/platform.png");
 
-    // obstacles
-    this.load.image('pipe_down','assets/pipe_down.png');
-    this.load.image('pipe_up','assets/pipe_up.png');
+  // load bird
+  blue_bird = loadImage("assets/blue_bird.png");
+  red_bird = loadImage("assets/red_bird.png");
 
-    // bird
-    this.load.spritesheet('red_bird',
-                          'assets/red_bird.png',
-                          { frameWidth: Params.game.BIRD_RADIUS, frameHeight: Params.game.BIRD_RADIUS });
+  // load pipes
+  pipe_down = loadImage("assets/pipe_down.png");
+  pipe_up = loadImage("assets/pipe_up.png");
 
-    this.load.spritesheet('blue_bird',
-                          'assets/blue_bird.png',
-                          { frameWidth: Params.game.BIRD_RADIUS, frameHeight: Params.game.BIRD_RADIUS });
+  var canvas = createCanvas(params.FRAME_WIDTH,params.FRAME_HEIGHT);
+  canvas.parent('game-container');
+  frameRate(params.FRAME_RATE);
 
-    // number
-    this.load.image('0','assets/0.png');
-    this.load.image('1','assets/1.png');
-    this.load.image('2','assets/2.png');
-    this.load.image('3','assets/3.png');
-    this.load.image('4','assets/4.png');
-    this.load.image('5','assets/5.png');
-    this.load.image('6','assets/6.png');
-    this.load.image('7','assets/7.png');
-    this.load.image('8','assets/8.png');
-    this.load.image('9','assets/9.png');
+  bird_test = new Bird('blue');
 }
 
-var platforms;
+function draw() {
+  // load static environment
+  background(bg);
+  image(grd,params.GROUND_X,params.GROUND_Y);
 
-function create() {
-    //this.physics.startSystem(Phaser.Physics.ARCADE);
-    this.add.image(Params.frame.FRAME_WIDTH/2,Params.frame.FRAME_HEIGHT/2,'background');
-    
-    platforms = this.physics.add.staticGroup();
-    platforms.create(Params.frame.GRD_POSX, Params.frame.GRD_POSY,'platform');
-
+  switch (game_state){
+    case 'prestart':
+          bird_test.hover();
+          break;
+    case 'running':
+          break;
+    case 'gameover':
+          break;
+  }
 
 }
-function update() {}
+
+function bird_hover(){
+
+}
+
+var Bird = function(color){
+    this.colour = color;
+    this.velocity = createVector(0, 0);
+    this.position = createVector(params.BIRD_X, params.BIRD_Y);
+    this.up = true; 
+    this.score = 0;
+};
+
+Bird.prototype.hover = function(){
+    if (this.position.y <= params.BIRD_Y - 5){
+      // too high
+      this.up = false;
+    }else if (this.position.y >= params.BIRD_Y + 5){
+      // too low
+      this.up = true;
+    }
+
+    if (this.up){
+      this.position.add(createVector(0, -1));
+    }else{
+      this.position.add(createVector(0, 1));
+    }
+
+    image(blue_bird,this.position.x,this.position.y);
+}
