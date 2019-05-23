ba4d10e4b8a73f0b871d8137548ba4c1312465d0
Hai Xun <haixun00@gmail.com>
2019-05-23 16:49:48 +0700

    introduce params

---
+++ js/params.js
@@ -0,0 +1,20 @@
+var Params = {
+
+    frame_updater: {
+        WIDTH_OF_SCREEN: 672,
+        HEIGHT_OF_SCREEN: 512
+    },
+
+    game_manager: {
+        NUM_OF_PIPES: 3,
+        WIDTH_OF_PIPE: 52,
+        HEIGHT_OF_PIPE: 500,
+        POS_MIN_Y_PIPE: 100,
+        POS_MAX_Y_PIPE: 305,
+        GAP_PIPE: 100,
+
+        
+    }
+
+
+};
